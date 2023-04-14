738d35be4f79385b140490a91beab47ed182ea85
Tomas alejandro Martinez cruz <tomartin@c1r17s6.42madrid.com>
2023-04-14 18:14:09 +0200

    prechange 07

--- module_00/07/filterwords.py
+++ module_00/07/filterwords.py
@@ -2,16 +2,19 @@ import argparse
 import string
 
 parser = argparse.ArgumentParser(description = 'removes all the words in a string that are shorter than or equal to n letters')
-parser.add_argument('string', type = str)
-parser.add_argument('number', type = int)
+parser.add_argument('string', type = str, nargs='?', default=None)
+parser.add_argument('number', type = int, nargs='?', default=None)
+
 args = parser.parse_args()
+
+if args.string is None or args.number is None:
+    print('ERROR')
+    exit()
 
 if __name__ == '__main__':
     st = args.string.translate(str.maketrans('', '', string.punctuation))
     work = st.split()
-    print(work)
-    for x in work:
-        print(x, "->", len(x))
+    for x in work.copy():
         if len(x) <= args.number:
             work.remove(x)
     print(work)

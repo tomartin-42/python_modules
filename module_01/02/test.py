0f90f9621e1ff6ba42f741873f24b7ba292d4e80
tommy <tommy@kali-tommy.tommy>
2023-04-17 18:38:41 +0200

    ex02 vector end

--- module_01/02/test.py
+++ module_01/02/test.py
@@ -22,3 +22,44 @@ v1 / 0.0
 2.0 / v1
 # Expected output:
 # NotImplementedError: Division of a scalar by a Vector is not defined here.
+
+# Example 1:
+v1 = Vector([[0.0], [1.0], [2.0], [3.0]])
+print(v1.shape)
+# Expected output:
+#(4,1)
+print(v1.T())
+# Expected output:
+# Vector([[0.0, 1.0, 2.0, 3.0]])
+print(v1.T().shape)
+# Expected output:
+# (1,4)
+# Example 2:
+v2 = Vector([[0.0, 1.0, 2.0, 3.0]])
+print(v2.shape)
+# Expected output:
+# (1,4)
+print(v2.T())
+# Expected output:
+# Vector([[0.0], [1.0], [2.0], [3.0]])
+print(v2.T().shape)
+# Expected output:
+# (4,1)
+
+# Example 1:
+v1 = Vector([[0.0], [1.0], [2.0], [3.0]])
+v2 = Vector([[2.0], [1.5], [2.25], [4.0]])
+print(v1.dot(v2))
+# Expected output:
+# 18.0
+v3 = Vector([[1.0, 3.0]])
+v4 = Vector([[2.0, 4.0]])
+print(v3.dot(v4))
+# Expected output:
+# 13.0
+v1
+# Expected output: to see what __repr__() should do
+# [[0.0, 1.0, 2.0, 3.0]]
+print(v1)
+# Expected output: to see what __str__() should do
+# [[0.0, 1.0, 2.0, 3.0]]
--- module_01/02/vector.py
+++ module_01/02/vector.py
@@ -5,9 +5,9 @@ class Vector:
     def __init__(self, *args):
         self.vector = []
         if len(args) == 1 and isinstance(args[0], int):
-            self.vector = [float(i) for i in range(args[0])]
+            self.vector = [[float(i)] for i in range(args[0])]
         elif len(args) == 2 and all(type(a) == int for a in args):
-            self.vector = [float(i) for i in range(args[0], args[1])]
+            self.vector = [[float(i)] for i in range(args[0], args[1])]
         else:
             for i in args[0]:
                 if isinstance(i, list):
@@ -16,8 +16,7 @@ class Vector:
                     self.vector.append([i])
             pass
         self.shape = (len(self.vector), len(self.vector[0]))
-        print(self.shape)
-
+    
     def dot(self, v2):
         if not isinstance(v2, Vector):
             raise TypeError("Arg is no a Vector class")
@@ -30,16 +29,17 @@ class Vector:
         return resp
 
     def T(self):
-        transposed = [([0.0] * self.shape[0]) for i in range(self.shape[1])]
-        for i in range(self.shape[0]):
-            for j in range(self.shape[1]):
-                transposed[j][i] += self.vector[i][j]
-        res_v = Vector(1)
-        res_v.vector = transposed
+        if self.shape[1] == 1:
+            # Si es un vector columna, retorna un vector fila
+            transposed = [self.vector[i][0] for i in range(self.shape[0])]
+        else:
+            # Si es una matriz, retorna la matriz transpuesta
+            transposed = [([0.0] * self.shape[0]) for i in range(self.shape[1])]
+            for i in range(self.shape[0]):
+                for j in range(self.shape[1]):
+                    transposed[j][i] = self.vector[i][j]
+        res_v = Vector(transposed)
         return res_v
-
-    def __repr__(self):
-        return str("Vector(" + str(self.vector) + ")")
 
     def __mul__(self, number):
         aux = copy.deepcopy(self)
@@ -51,6 +51,37 @@ class Vector:
                 for j in range(len(aux.vector[i])):
                     aux.vector[i][j] *= number
         return aux
+
+    def __rmul__(self, number):
+        return self.__mul__(number)
+
+    def __add__(self, v2):
+        if not isinstance(v2, Vector):
+            raise TypeError("Arg is no a Vector class")
+        if self.shape != v2.shape:
+            raise ValueError("Vectors not equal shape")
+        aux = copy.deepcopy(self)
+        for i in range(len(aux.vector)):
+            for j in range(len(aux.vector[i])):
+                aux.vector[i][j] += v2.vector[i][j]
+        return aux
+
+    def __radd__(self, v2):
+        return self.__add__(v2)
+
+    def __sub__(Self, v2):
+        if not isinstance(v2, Vector):
+            raise TypeError("Arg is no a Vector class")
+        if self.shape != v2.shape:
+            raise ValueError("Vectors not equal shape")
+        aux = copy.deepcopy(self)
+        for i in range(len(aux.vector)):
+            for j in range(len(aux.vector[i])):
+                aux.vector[i][j] -= v2.vector[i][j]
+        return aux
+    
+    def __rsub__(self, v2):
+        return self.__sub__(v2)
 
     def __truediv__(self, number):
         try:
@@ -68,23 +99,28 @@ class Vector:
                 for j in range(len(aux.vector[i])):
                     aux.vector[i][j] /= number
         return aux
+    
+    def __rtruediv__(self, number):
+        print("Division of a scalar by a Vector is not defined here.")
 
     def __str__(self):
         return str(self.vector)
 
+    def __repr__(self):
+        return str("Vector(" + str(self.vector) + ")")
+
 
 if __name__ == '__main__':
+    """
     a = Vector(2)
     b = Vector([1.0], [1.5], [2.0], [2.5])
     c = Vector([1.0, 1.5])
     d = Vector(15, 29)
     e = Vector([0.0, 1.0])
-    """
     print(b.shape)
     print(b)
     print(c.shape)
     print(c)
-    """
   # print(b.dot(a))
   # print(a.T())
   # print(e)
@@ -93,3 +129,4 @@ if __name__ == '__main__':
     print(c)
     print(d)
     print(e)
+    """
